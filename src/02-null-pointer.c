72cccf5ffc6e84289fae4655d2f468efa8b5f60f
Ben Pratt <benjaminrpratt@gmail.com>
2024-09-17 16:57:16 +0000

    print pointer address

--- src/02-null-pointer.c
+++ src/02-null-pointer.c
@@ -2,8 +2,8 @@
 
 int main(void) {
     int *ptr = NULL;
-    int value = *ptr;
 
-    printf("Huh, I got 0x%08x\n", value);
+    printf("I'm going to dereference 0x%016lx\n", (long)ptr);
+    printf("Huh, I got 0x%08x\n", *ptr);
     return 0;
 }
